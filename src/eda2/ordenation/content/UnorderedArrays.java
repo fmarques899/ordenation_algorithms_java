1b4c6f586d99d8ff48541e853489be81ea6847cc
fmarques55 <fmarques899@gmail.com>
2016-09-05 20:11:04 -0300

    Creating java project

---
+++ src/eda2/ordenation/content/UnorderedArrays.java
@@ -0,0 +1,5 @@
+package eda2.ordenation.content;
+
+public class UnorderedArrays {
+	public static final int unorderedArray[] = {63,12,14,53,37,92,22,41,71,1,98,75,37,26,64,13,45,72,88,2,5,24,77,55,33,61,59,43,11,19,71,95,38,44};
+}
---
+++ src/eda2/ordenation/main/SelectionSort.java
@@ -0,0 +1,19 @@
+package eda2.ordenation.main;
+
+public class SelectionSort {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+	
+	private int ordenateBySelectionSort(int array[]){
+		int minimumValue;
+		for(int i=0; i < array.length; i++){
+			
+		}
+		
+		return 1;
+	}
+}
